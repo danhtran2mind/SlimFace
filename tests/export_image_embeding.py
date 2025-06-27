ae0daf6e540ee06735826ca37bb10034ef1c8bfe
Danh Tran <cprmmb99@gmail.com>
2025-06-27 18:00:55 +0000

    Inital commit

---
+++ tests/export_image_embeding.py
@@ -0,0 +1,28 @@
+import torch
+from torchvision import transforms
+
+# Import edgeface modules using relative paths
+from edgeface.face_alignment import align
+from edgeface.backbones import get_model
+
+if __name__ == "__main__":
+    batch_size = 1
+    # load model
+    model_name="edgeface_s_gamma_05" # or edgeface_xs_gamma_06
+    model=get_model(model_name)
+    checkpoint_path=f'./edgeface/checkpoints/{model_name}.pt'
+    model.load_state_dict(torch.load(checkpoint_path, map_location='cpu')) # Load state dict
+    model.eval() # Call eval() on the model object
+
+    transform = transforms.Compose([
+                transforms.ToTensor(),
+                transforms.Normalize(mean=[0.5, 0.5, 0.5], std=[0.5, 0.5, 0.5]),
+                ])
+
+    path = 'tests/test_images/Elon_Musk.jpg'
+    aligned = align.get_aligned_face(path) # align face
+    transformed_input = transform(aligned) # preprocessing
+    transformed_input = transformed_input.reshape(batch_size, *transformed_input.shape)
+    # extract embedding
+    embedding = model(transformed_input)
+    print(embedding.shape)
