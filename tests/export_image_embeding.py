9ba5ae04351237475c09be452b21ca339ed05f56
Danh Tran <cprmmb99@gmail.com>
2025-06-28 01:24:36 +0700

    Update export_image_embeding.py

--- tests/export_image_embeding.py
+++ tests/export_image_embeding.py
@@ -1,3 +1,4 @@
+import os
 import torch
 from torchvision import transforms
 
@@ -8,10 +9,12 @@ from edgeface.backbones import get_model
 if __name__ == "__main__":
     batch_size = 1
     # load model
-    model_name="edgeface_s_gamma_05" # or edgeface_xs_gamma_06
+    model_name = "edgeface_s_gamma_05.pt" # or edgeface_xs_gamma_06
     model=get_model(model_name)
-    checkpoint_path=f'./edgeface/checkpoints/{model_name}.pt'
+    checkpoint_path = os.path.join("edgeface", "checkpoints", model_name)
+
     model.load_state_dict(torch.load(checkpoint_path, map_location='cpu')) # Load state dict
+    
     model.eval() # Call eval() on the model object
 
     transform = transforms.Compose([
