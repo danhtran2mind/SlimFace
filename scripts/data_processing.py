e1db50e2d09f6c17eb12f466d697deadcc59309f
Danh Tran <cprmmb99@gmail.com>
2025-07-02 20:20:20 +0700

    Update data_processing.py

--- scripts/data_processing.py
+++ scripts/data_processing.py
@@ -27,7 +27,7 @@ def process_image(src_path, dest_dir, aug=None):
         
         # Apply augmentation if specified and save augmented image
         if aug:
-            print("Augmentaion")
+            # print("Augmentaion")
             img_array_aug = aug.augment_image(img_array)
             # Clip values to ensure valid range after augmentation
             img_array_aug = np.clip(img_array_aug, 0, 1)
@@ -35,6 +35,7 @@ def process_image(src_path, dest_dir, aug=None):
             aug_img = Image.fromarray((img_array_aug * 255).astype(np.uint8))
             # Save augmented image with '_aug' suffix
             aug_dest_path = os.path.join(dest_dir, f"aug_{os.path.basename(src_path)}")
+            print("Save to: ", aug_dest_path)
             aug_img.save(aug_dest_path, quality=100)
     except Exception as e:
         print(f"Error processing image {src_path}: {e}")
