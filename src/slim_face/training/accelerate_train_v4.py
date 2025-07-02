131a77e1b5d9b745e9595ba2c75b90c56432b652
Danh Tran <cprmmb99@gmail.com>
2025-07-02 16:44:56 +0700

    Update accelerate_train_v4.py

--- src/slim_face/training/accelerate_train_v4.py
+++ src/slim_face/training/accelerate_train_v4.py
@@ -95,13 +95,11 @@ class FaceClassifier(nn.Module):
         self.base_model = base_model
         self.fc = nn.Sequential(
             nn.Linear(embedding_dim, 512),
-            nn.BatchNorm1d(512),
             nn.ReLU(),
-            nn.Dropout(0.2),
+            nn.Dropout(0.5),
             nn.Linear(512, 256),
-            nn.BatchNorm1d(256),
             nn.ReLU(),
-            nn.Dropout(0.2),
+            nn.Dropout(0.5),
             nn.Linear(256, num_classes)
         )
 
