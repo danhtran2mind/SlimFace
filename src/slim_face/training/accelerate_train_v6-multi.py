aaa275add5e77acbca6ad7cb23f176fa6cdcd2d9
Danh Tran <cprmmb99@gmail.com>
2025-07-03 22:28:42 +0700

    Update accelerate_train_v6-multi.py

--- src/slim_face/training/accelerate_train_v6-multi.py
+++ src/slim_face/training/accelerate_train_v6-multi.py
@@ -32,6 +32,7 @@ def resolve_path(path):
     """Convert a string like 'module.submodule.function' to a Python callable object."""
     try:
         module_name, obj_name = path.rsplit('.', 1)
+        print("module_name, obj_name: ", module_name, obj_name)
         module = __import__("torchvision", module_name, fromlist=[obj_name])
         return getattr(module, obj_name)
     except Exception as e:
